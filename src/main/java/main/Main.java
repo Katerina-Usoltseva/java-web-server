0d7c697bc55d47bbcb88ab50c6d68b010f0a2fa3
Katerina Usoltseva <usoltsevaekaterinaa@gmail.com>
2019-12-03 14:38:11 +0700

    Added a simple solution for GET-request and building jar

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        Logger.getGlobal().info("Server started");
+        server.join();
+    }
+}
