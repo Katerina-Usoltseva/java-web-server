3bb2353acf8e1c406068df834f92d1e49d31bc67
Katerina Usoltseva <usoltsevaekaterinaa@gmail.com>
2020-01-07 18:56:53 +0700

    Added servlets support

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,17 +1,25 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
+
 import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
 
         Server server = new Server(8080);
         server.setHandler(context);
